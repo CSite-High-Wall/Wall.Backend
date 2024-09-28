b59e184edae67ca2a3efff5e6d0cad0e36da1631
16jialuo <453058083@qq.com>
2024-09-28 14:39:36 +0800

    fix:评论接口更正，ctx去除

--- internal/controller/review.go
+++ internal/controller/review.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"errors"
 	"wall-backend/internal/model"
 	"wall-backend/internal/service"
 	"wall-backend/pkg/utils"
@@ -10,73 +11,97 @@ import (
 )
 
 type ReviewController struct {
-	reviewService service.ReviewService
+	userService       service.UserService
+	expressionService service.ExpressionService
+	reviewService     service.ReviewService
 }
 
-func NewReviewController(service service.ReviewService) ReviewController {
+func NewReviewController(userService service.UserService, reviewservice service.ReviewService, expressionService service.ExpressionService) ReviewController {
 	return ReviewController{
-		reviewService: service,
+		reviewService:     reviewservice,
+		expressionService: expressionService,
+		userService:       userService,
 	}
 }
 
 // 发布评论接口
 func (controller ReviewController) Publish(c *gin.Context) {
 	var requestBody model.ReviewCreateRequestJsonObject
+	var userId = utils.ParseUserIdFromRequest(c)
 	if err := c.BindJSON(&requestBody); err != nil {
 		utils.ResponseFailWithoutData(c, "missing parameters")
 		return
 	}
-	_, err := controller.reviewService.FindPostByPostId(requestBody.ExpressionId)
-	if err != nil {
-		if err == gorm.ErrRecordNotFound {
-			utils.ResponseFailWithoutData(c, "帖子不存在")
-			return
+	_, error := controller.userService.FindUserByUserId(userId)
+	if errors.Is(error, gorm.ErrRecordNotFound) {
+		utils.ResponseFailWithoutData(c, "未找到该用户") // 检查用户
+	} else if error != nil {
+		utils.ResponseFailWithoutData(c, "获取用户信息失败") // 检查用户
+	} else {
+		_, error := controller.expressionService.FindExpressionByExpressionId(requestBody.ExpressionId)
+		if errors.Is(error, gorm.ErrRecordNotFound) {
+			utils.ResponseFailWithoutData(c, "未找到该表白帖子") // 检查表白帖子
+		} else if error != nil {
+			utils.ResponseFailWithoutData(c, "获取表白帖子信息失败") // 检查表白帖子
+		} else if error := controller.reviewService.Publish(userId, requestBody); error != nil {
+			utils.ResponseFailWithoutData(c, "发布评论失败")
 		} else {
-			utils.ResponseFailWithoutData(c, "评论失败")
-			return
+			utils.ResponseOkWithoutData(c)
 		}
 	}
-	utils.ResponseOkWithoutData(c)
 }
 
 // 删除评论接口
 func (controller ReviewController) Delete(c *gin.Context) {
 	var requestBody model.ReviewDeleteRequestJsonObject
+	var userId = utils.ParseUserIdFromRequest(c)
 	if err := c.BindJSON(&requestBody); err != nil {
 		utils.ResponseFailWithoutData(c, "missing parameters")
 		return
 	}
-	_, err := controller.reviewService.FindReviewByReviewId(requestBody.ID)
-	if err != nil {
-		utils.ResponseFailWithoutData(c, "评论不存在")
-		return
+	_, error := controller.userService.FindUserByUserId(userId)
+	if errors.Is(error, gorm.ErrRecordNotFound) {
+		utils.ResponseFailWithoutData(c, "未找到该用户") // 检查用户
+	} else if error != nil {
+		utils.ResponseFailWithoutData(c, "获取用户信息失败") // 检查用户
+	} else {
+		review, error := controller.reviewService.FindReviewByReviewId(requestBody.ID)
+		if error != nil {
+			utils.ResponseFailWithoutData(c, "获取评论信息失败")
+		} else if review.UserId != userId {
+			utils.ResponseFailWithoutData(c, "您只能删除自己的评论") // 判断创建评论的用户和请求者是否为同一人
+		} else if error = controller.reviewService.Delete(userId, requestBody); error != nil {
+			utils.ResponseFailWithoutData(c, "删除表白失败") // 删除评论失败
+		} else {
+			utils.ResponseOkWithoutData(c) // 返回成功相应
+		}
 	}
-	_, err = controller.reviewService.FindReviewByUserId(requestBody.UserId, requestBody.ID)
-	if err != nil {
-		utils.ResponseFailWithoutData(c, "不是本人操作")
-		return
-	}
-	utils.ResponseOkWithoutData(c)
 
 }
 
 // 更新评论接口
 func (controller ReviewController) Edit(c *gin.Context) {
 	var requestBody model.ReviewUpdateRequestJsonObject
+	var userId = utils.ParseUserIdFromRequest(c)
 	if err := c.BindJSON(&requestBody); err != nil {
 		utils.ResponseFailWithoutData(c, "missing parameters")
 		return
 	}
-	_, err := controller.reviewService.FindReviewByReviewId(requestBody.ID)
-	if err != nil {
-		utils.ResponseFailWithoutData(c, "评论不存在")
-		return
+	_, error := controller.userService.FindUserByUserId(userId)
+	if errors.Is(error, gorm.ErrRecordNotFound) {
+		utils.ResponseFailWithoutData(c, "未找到该用户") // 检查用户
+	} else if error != nil {
+		utils.ResponseFailWithoutData(c, "获取用户信息失败") // 检查用户
+	} else {
+		review, error := controller.reviewService.FindReviewByReviewId(requestBody.ID)
+		if error != nil {
+			utils.ResponseFailWithoutData(c, "获取评论信息失败")
+		} else if review.UserId != userId {
+			utils.ResponseFailWithoutData(c, "您只能删除自己的评论") // 判断创建评论的用户和请求者是否为同一人
+		} else if error = controller.reviewService.Edit(userId, requestBody); error != nil {
+			utils.ResponseFailWithoutData(c, "修改表白失败") // 修改评论失败
+		} else {
+			utils.ResponseOkWithoutData(c) // 返回成功相应
+		}
 	}
-	_, err = controller.reviewService.FindReviewByUserId(requestBody.UserId, requestBody.ID)
-	if err != nil {
-		utils.ResponseFailWithoutData(c, "不是本人操作")
-		return
-	}
-	utils.ResponseOkWithoutData(c)
-
 }
